d961703c20d61c3192d5ba40b9e75aa1d06739a4
BuildTools <spikespaz@outlook.com>
2020-03-12 13:45:05 -0700

    Add basic material items

--- src/main/java/com/spikespaz/essentialadditions/EssentialAdditions.java
+++ src/main/java/com/spikespaz/essentialadditions/EssentialAdditions.java
@@ -3,12 +3,14 @@ package com.spikespaz.essentialadditions;
 import net.fabricmc.api.ModInitializer;
 
 public class EssentialAdditions implements ModInitializer {
+	public static final String MODID = "essential-additions";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/spikespaz/essentialadditions/ModItems.java
@@ -0,0 +1,26 @@
+package com.spikespaz.essentialadditions;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public final class ModItems {
+    public static final Item OBSIDIAN_SHARD = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item SULFUR = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item CHARCOAL_DUST = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item RUBY = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item IRON_PLATING = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item GOLD_PLATING = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item CHAIN_LINK = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
+    public static void registerModItems() {
+        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "obsidian_shard"), OBSIDIAN_SHARD);
+        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "sulfur"), SULFUR);
+        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "charcoal_dust"), CHARCOAL_DUST);
+        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "ruby"), RUBY);
+        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "iron_plating"), IRON_PLATING);
+        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "gold_plating"), GOLD_PLATING);
+        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "chain_link"), CHAIN_LINK);
+    }
+}
