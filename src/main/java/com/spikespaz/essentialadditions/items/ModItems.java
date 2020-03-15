509f39ec2571ce2e8eba17dafd733be42a145d29
BuildTools <spikespaz@outlook.com>
2020-03-14 19:48:18 -0700

    Add new registration logic for items

--- src/main/java/com/spikespaz/essentialadditions/items/ModItems.java
+++ src/main/java/com/spikespaz/essentialadditions/items/ModItems.java
@@ -1,10 +1,17 @@
 package com.spikespaz.essentialadditions.items;
 
 import com.spikespaz.essentialadditions.EssentialAdditions;
+import com.spikespaz.essentialadditions.blocks.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class ModItems {
     public static final Item OBSIDIAN_SHARD = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
@@ -15,13 +22,18 @@ public final class ModItems {
     public static final Item GOLD_PLATING = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
     public static final Item CHAIN_LINK = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 
-    public static void registerModItems() {
-        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "obsidian_shard"), OBSIDIAN_SHARD);
-        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "sulfur"), SULFUR);
-        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "charcoal_dust"), CHARCOAL_DUST);
-        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "ruby"), RUBY);
-        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "iron_plating"), IRON_PLATING);
-        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "gold_plating"), GOLD_PLATING);
-        Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, "chain_link"), CHAIN_LINK);
+    public static void registerModItems() throws IllegalAccessException {
+        Field[] fields = ModBlocks.class.getDeclaredFields();
+
+        for (Field field : fields) {
+            Class type = field.getType();
+
+            if (type != Item.class)
+                continue;
+
+            Item item = (Item) field.get(null);
+
+            Registry.register(Registry.ITEM, new Identifier(EssentialAdditions.MODID, field.getName().toLowerCase()), item);
+        }
     }
 }
